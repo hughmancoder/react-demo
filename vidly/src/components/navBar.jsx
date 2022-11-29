6d76b7d4e4d4d2947eb141b27708fe2a0e4208df
hughmancoder <hugh.signoriello@gmail.com>
2022-11-29 13:48:46 +1100

    form components created

--- vidly/src/components/navBar.jsx
+++ vidly/src/components/navBar.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Link, NavLink } from "react-router-dom";
 
 const NavBar = () => {
-  // sourced from bootsrap
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <Link className="navbar-brand" to="/">
@@ -29,6 +28,12 @@ const NavBar = () => {
           </NavLink>
           <NavLink className="nav-item nav-link" to="/rentals">
             Rentals
+          </NavLink>
+          <NavLink className="nav-item nav-link" to="/login">
+            Login
+          </NavLink>
+          <NavLink className="nav-item nav-link" to="/register">
+            Register
           </NavLink>
         </div>
       </div>
